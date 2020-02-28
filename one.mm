378ea37a72db13b8f9c5a837fca05b7fafb87169
mango <690852195@qq.com>
2020-02-28 08:44:41 +0800

    commit one

---
+++ one.mm
@@ -0,0 +1,60 @@
+
+红黑树必须满足以下5个条件
+
+1> 根节点必须是黑色
+2> 节点的颜色必须是黑色 或者 红色
+3> 叶子节点可以是黑色或者红色, 假想节点(null 节点) 也必须是黑色  ?
+4> red 节点的子节/父节点点都是黑色
+5> 从任一节点到叶子节点的所有路劲都包含相同数目的 黑节点
+   且 从任一节点到假想节点(null)的所有路径包含相同数目的黑节点
+
+
+
+
+红黑树与B树的类推
+1>红黑树和 4 阶B树 (2-3-4树) 具有等价性
+2>黑色节点与他的红色子节点融合在一起, 形成一个B数的节点, 如果没有红色子节点就独立称为一个节点
+3> 从上面2第二条融合的概念可以推断出, 红色树中, 黑色节点(非null节点)的个数就是 4阶B数的总节点树
+4> 红色树只能和4阶 B树进行类比, 如果是其它阶的B树相比较都是不严谨的
+
+
+
+
+红黑树添加子元素
+
+1> 红黑树添加的叶子节点好为红色
+
+1:05
+
+
+
+ffmpeg -y -noautorotate -ss 0 -i abc.mov -c:v libx264 -profile:v high -level 3.1 -keyint_min 1 -g 50 -crf 21 -refs 3 -trellis 1 -me_method umh -subq 9  -s 1280x720 -r 25 -c:a libfdk_aac -ar 44100 -b:a 128000 -sws_flags area -movflags +faststart output.mp4
+
+
+
+
+ffmpeg -i C:\Users\10375\Desktop\learner-demo.m4v -b:v 400k -s 960x540 newfiles/learner-demo.mp4
+
+
+
+
+
+
+
+cocoapods-deintegrate (1.0.4, 1.0.2)
+cocoapods-downloader (1.2.2)
+cocoapods-plugins (1.0.0)
+cocoapods-search (1.0.0)
+cocoapods-stats (1.0.0)
+cocoapods-trunk (1.4.1, 1.3.1)
+cocoapods-try (1.1.0)
+
+
+
+
+
+
+
+
+
+
